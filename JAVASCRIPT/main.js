b34a6247e1b4478d1a96fef00dec41044baa21c0
Flash Lavon <flash.lavon@hotmail.com>
2023-11-16 22:36:09 +0700

    feat: fungsi mapping product ke view ketika response api get product didapatkan (2-js)

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -42,11 +42,32 @@ getAllProducts = async () => {
         const response = await fetch("https://6554347063cafc694fe63a4b.mockapi.io/api/v1/products");
         const data = await response.json();
         console.log(data);
+        mappingProduct(data);
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
 mappingProduct = (resource) => {
+    resource.forEach(element => {
+        const tempElement = document.createElement('div');
 
+        const productTemplate = `
+        <div class="border rounded overflow-hidden flex-md-row mb-4 post">
+            <div class="col p-4 d-flex flex-column position-static">
+                <strong class="d-inline-block mb-2 text-success">$ ${element.price}</strong>
+                <h3 class="mb-0">${element.name}</h3>
+                <div class="mb-1 text-muted">${element.createdAt}</div>
+                <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                <a href="#">Continue reading</a>
+            </div>
+        </div>
+        `;
+
+        tempElement.innerHTML = productTemplate;
+        tempElement.id = `product${element.id}`;
+        tempElement.classList.add("col-md-4");
+        containerProducts.appendChild(tempElement);
+
+    });
 }
